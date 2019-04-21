e6e13e4a7922b6cabffcf9fb4869c7dc68e4249f
PaulCaudal <47568042+PaulCaudal@users.noreply.github.com>
2019-04-21 21:45:13 +0200

    Add files via upload

--- graphe.h
+++ graphe.h
@@ -3,6 +3,7 @@
 #include"sommet.h"
 #include"svgfile.h"
 #include"arrete.h"
+#include"pareto.h"
 #include<vector>
 
 
@@ -13,7 +14,9 @@ public:
     void dessiner(Svgfile& svgout,int);
     std::vector<Arrete> kruskalp1();
     std::vector<Arrete> kruskalp2();
+    float dijkstra(std::vector<Arrete>);
     void kruskal(int);
+    void solution2(Svgfile& svgout,Pareto& pareto);
 protected:
     std::vector<Sommet> m_sommet;
     std::vector<Arrete> m_arrete;

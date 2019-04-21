ba19b51402b20620c400566d2996b91bbd6e82b9
PaulCaudal <47568042+PaulCaudal@users.noreply.github.com>
2019-04-21 22:35:04 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@ int main()
 {
     std::string fichier1, fichier2;
     Svgfile svgout;
+    Pareto pareto;
     int r;
-    std::cout << "saisir les noms des fichiers" << std::endl;
-    std::cin>>fichier1>>fichier2;
-    graphe g{fichier1, fichier2};
+    //std::cout << "saisir les noms des fichiers" << std::endl;
+    //std::cin>>fichier1>>fichier2;
+    //graphe g{fichier1, fichier2};
+    graphe g{"broadway.txt","broadway_weights_0.txt"};
     std::cin>>r;
     if(r!=0)
     {
         g.kruskal(r);
     }
     g.dessiner(svgout,r);
+    g.solution(svgout,pareto);
+
+    pareto.dessiner(svgout);
     return 0;
 }
